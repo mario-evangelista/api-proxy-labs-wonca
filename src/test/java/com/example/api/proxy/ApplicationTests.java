99ec23da1a772cc041f50e46c8f5d6d5e945a893
Mário Evangelista <mariojbe@outlook.com>
2025-05-25 01:18:09 -0300

    DEPLOY RENDER

--- src/test/java/com/example/api/proxy/ApplicationTests.java
+++ src/test/java/com/example/api/proxy/ApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.api.proxy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }

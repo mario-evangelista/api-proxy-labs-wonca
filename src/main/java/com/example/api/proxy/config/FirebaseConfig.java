06b893b9ae8f7666771dc9a1a523cc2dc43dd613
Mário Evangelista <mariojbe@outlook.com>
2025-05-24 22:22:50 -0300

    PUSH NOTIFICATION

--- src/main/java/com/example/api/proxy/config/FirebaseConfig.java
+++ src/main/java/com/example/api/proxy/config/FirebaseConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -16,7 +17,8 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseMessaging firebaseMessaging() throws IOException {
-        InputStream serviceAccount = new ClassPathResource("firebase/serviceAccountKey.json").getInputStream();
+        //InputStream serviceAccount = new ClassPathResource("firebase/serviceAccountKey.json").getInputStream();
+        FileInputStream serviceAccount = new FileInputStream(System.getenv("FIREBASE_CREDENTIALS"));
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))

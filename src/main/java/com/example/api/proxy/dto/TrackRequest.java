d20ea2a5d53d3f33caa95f80ddd23aa896dd0812
Mário Evangelista <121322767+mario-evangelista@users.noreply.github.com>
2025-05-28 21:44:12 -0300

    Update TrackRequest.java

--- src/main/java/com/example/api/proxy/dto/TrackRequest.java
+++ src/main/java/com/example/api/proxy/dto/TrackRequest.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.Pattern;
 @Schema(description = "Objeto para requisição de rastreamento")
 public class TrackRequest {
     @Schema(description = "Código de rastreamento do pacote", example = "AB123456789BR")
-    //@NotNull(message = "Código de rastreamento é obrigatório")
     @Pattern(regexp = "[A-Z]{2}\\d{9}[A-Z]{2}", message = "Código de rastreamento deve seguir o formato XX999999999XX")
     private String code;
 
